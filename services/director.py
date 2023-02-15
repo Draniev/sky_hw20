0dbcfa62fc970e006f87e067c138ccec27784242
Сергей Попов <sergan_p@mail.ru>
2023-02-16 02:09:50 +0300

    director and genre service update

--- services/director.py
+++ services/director.py
@@ -11,3 +11,18 @@ class DirectorService:
 
     def get_all(self) -> list[Director]:
         return self.director_dao.get_all()
+
+    def create(self, director_data: dict) -> Director:
+        return self.director_dao.create(director_data)
+
+    def update(self, director_data: dict) -> Director:
+        director = self.director_dao.get_one(director_data['id'])
+        keys_4_update = director_data.keys()
+
+        if 'name' in keys_4_update:
+            director.name = director_data['name']
+
+        return self.director_dao.update(director)
+
+    def delete(self, did: int):
+        return self.director_dao.delete(did)
--- services/genre.py
+++ services/genre.py
@@ -12,3 +12,17 @@ class GenreService:
     def get_all(self) -> list[Genre]:
         return self.genre_dao.get_all()
 
+    def create(self, genre_data: dict) -> Genre:
+        return self.genre_dao.create(genre_data)
+
+    def update(self, genre_data: dict) -> Genre:
+        genre = self.genre_dao.get_one(genre_data['id'])
+        keys_4_update = genre_data.keys()
+
+        if 'name' in keys_4_update:
+            genre.name = genre_data['name']
+
+        return self.genre_dao.update(genre)
+
+    def delete(self, gid: int) -> Genre:
+        return self.genre_dao.delete(gid)
